57554dc0194ec41f27fc8f744f6f6cd79bb9253f
Proger4 <37503927+Proger4@users.noreply.github.com>
2018-03-18 22:12:09 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_db',
-        'USER' : 'django',
-        'PASSWORD' : 'zvfntvfnbr22',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         } 
         
 }
